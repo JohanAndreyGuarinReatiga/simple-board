4a29e7db10c39aa7309e190010447b32f7129363
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-03-05 17:52:20 -0500

    feat: :construction: students saving form and table done

--- js/formStudent.js
+++ js/formStudent.js
@@ -1,4 +1,4 @@
-export const loadStudent = () => {
+export const loadStudent = ()=>{
     const DB = (localStorage.getItem("students")) ? JSON.parse(localStorage.getItem("students")) : [];
     return DB;
 }
@@ -6,11 +6,12 @@ export const loadStudent = () => {
 export const save = (data)=>{
     const DB = loadStudent();
     DB.push(data);
+    showRowsTable([data]);
     localStorage.setItem("students", JSON.stringify(DB));
-    return {status: 201, message: "El estudiante ${data.name} fue registrado de forma exitosa."}
+    return {status: 201, message: `El estudiante ${data.name} fue registrado exitosamente.`}
 }
 
-export const transformInputFormulary = (e) => {
+export const transformInputFormulary =  (e)=>{
     const data = Object.fromEntries(new FormData(e.target));
     data.phone = Number(data.phone)
     data.enroll_number = Number(data.enroll_number)
@@ -18,3 +19,44 @@ export const transformInputFormulary = (e) => {
     return data;
 }
 
+export const showRowsTable = (DB)=>{
+    for (let i = 0; i < DB.length; i++) {
+        const tr = document.createElement("tr")
+        const tdImage = document.createElement("td");
+        const imgStudent = document. createElement("img");
+        imgStudent.src = "#"
+        imgStudent.alt = "student";
+        tdImage.append(imgStudent);
+        const tdName = document.createElement("td");
+        tdName.textContent = DB[i].name;
+        const tdEmail = document.createElement("td");
+        tdEmail.textContent = DB[i].email;
+        const tdPhone = document.createElement("td");
+        tdPhone.textContent = DB[i].phone;
+        const tdEnrollNumber = document.createElement("td");
+        tdEnrollNumber.textContent = DB[i].enroll_number;
+        const tdDate_of_admission = document.createElement("td");
+        tdDate_of_admission.textContent = DB[i].date_of_admission;
+        tr.append(tdImage, tdName, tdEmail, tdPhone, tdEnrollNumber, tdDate_of_admission);
+        table__student.append(tr);
+    }
+
+    // let plantilla = "";
+    // for (let i = 0; i < DB.length; i++) {
+    //     plantilla += `
+    //         <tr>
+    //             <td><img src="#" alt="student"></td>
+    //             <td>Karthi</td>
+    //             <td>karthi@gmmail.com</td>
+    //             <td>7305477760</td>
+    //             <td>1234567305477760</td>
+    //             <td>08-Dec, 2021</td>
+    //             <td class="td__actions">
+    //                 <span class="span__edit">✏️</span>
+    //                 <span class="span__delete">🗑️</span>
+    //             </td>
+    //         </tr>
+    //     `;
+    // }
+    // console.log(plantilla);
+}
--- js/student.js
+++ js/student.js
@@ -1,18 +1,24 @@
-import {save, transformInputFormulary} from "./formStudent.js";
+import {save, transformInputFormulary, loadStudent, showRowsTable} from "./formStudent.js";
 
-const add_student = document.querySelector("#add_student");
+const add__student = document.querySelector("#add__student");
 const dialog__add_student = document.querySelector("#dialog__add_student");
 const dialog__close = document.querySelector("#dialog__close");
 const form__student = document.querySelector("#form__student");
+const table__student = document.querySelector("#table__student")
 
-add_student.addEventListener("click", () => {
-  dialog__add_student.showModal();
+
+showRowsTable(loadStudent());
+
+
+
+add__student.addEventListener("click", ()=>{
+    dialog__add_student.showModal();
 })
 
 dialog__close.addEventListener("click", ()=> dialog__add_student.close());
 
-form__student.addEventListener("submit", (e) => {
-    e.presetDefault();
+form__student.addEventListener("submit", (e)=>{
+    e.preventDefault();
     const data = transformInputFormulary(e)
     const response = save(data);
     console.log(response);
