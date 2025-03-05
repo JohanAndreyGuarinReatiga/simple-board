9aeb2c72f058cdb4d84a86c5a595e3452748b087
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-03-04 21:15:33 -0500

    feat: :sparkles: Valid form with its messages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import {users,validation} from "./credenciales.js";
+import {findDataByForm} from "./formSignIn.js";
 
 const form__login = document.querySelector("#form__login");
-form__login.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.target));
-    console.log(validation(users, data));
-})
+const dialog__login = document.querySelector("#dialog__login");
+const dialog__login_close = document.querySelector("#dialog__login_close");
+
+form__login.addEventListener("submit", findDataByForm) // Al dar click al boton de formularion se ejecuta la funcion findDataByForm
+dialog__login_close.addEventListener("click", (e) => dialog__login.close() ) // Al dar click a icono x del dialogo lo cierra 
